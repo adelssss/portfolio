5fc828ae3826e668c6364be75f1650ee8a65a1ad
adelssss <166361844+adelssss@users.noreply.github.com>
2024-11-17 18:10:23 +0500

    Update db.js

--- db.js
+++ db.js
@@ -1,13 +1,6 @@
 const mongoose = require('mongoose');
 
-const connectDB = async () => {
-  try {
-    const conn = await mongoose.connect(process.env.MONGO_URL); 
-    console.log(`MongoDB connected successfully`);
-  } catch (error) {
-    console.error(`Error: ${error.message}`);
-    process.exit(1); 
-  }
-};
-
-module.exports = connectDB;
+mongoose.connect(process.env.MONGODB_URI, { 
+  
+}).then(() => console.log('MongoDB connected'))
+  .catch(err => console.log('MongoDB connection error:', err));
